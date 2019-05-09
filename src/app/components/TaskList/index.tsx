cf6950fc073853f658b2c98772a766ef7ab09731
GrayStrider <zhoga.ivan@gmail.com>
2019-05-09 22:44:44 +1100

    doesn't update on list change, probably because it's stateless. GOnna try react-async

--- src/app/components/TaskList/index.tsx
+++ src/app/components/TaskList/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-
 import { map } from 'lodash';
 import { Wrapper } from './styles';
 import Task from '../Task';
@@ -8,33 +7,44 @@ import Scrollbar from '../Scrollbar';
 import { getCurrentListTasks2 } from 'app/components/TaskList/selectors';
 import { Loader } from 'semantic-ui-react';
 import { AsyncReactor } from '@ovotech/async-reactor-ts';
+import { Aigle } from 'aigle';
+import { list } from 'postcss';
 
+// @ts-ignore
+Aigle.mixin(require('lodash'));
 
 function TaskList(props) {
   const { filteredTasks } = props;
-  const wait = ms => new Promise((r, j)=>setTimeout(r, ms))
-  const mapFilteredTasks = () => Promise.all(map(filteredTasks,
+  const sleep = m => new Promise(r => setTimeout(r, m));
+
+  let asyncTasks = map(filteredTasks,
     async (task) => (
       <Task taskID={task.id} key={task.id}/>
-    )))
+    ));
+
   const asyncWrapper = (
     <AsyncReactor
-      loader={async () => await wait(5000)}>
-      {({loading, result}) => {
+      loader={async () => {
+        console.time('Slept for');
+        await sleep(3000);
+        console.timeEnd('Slept for');
+        return JSON.stringify(
+          filteredTasks
+        );
+      }}>
+      {({ loading, result }) => {
         if (loading) {
-          return <div>Loading...</div>
+          return <div>Loading...</div>;
         }
 
-        return <div>{result}</div>
+        return <div>{result}</div>;
       }}
     </AsyncReactor>
-  )
-  const ListWrapper = <>
+  );
+  const listWrapper = <>
     {
       map(filteredTasks,
-        (task) => (
-          <Task taskID={task.id} key={task.id}/>
-        ))
+        (task) => <Task taskID={task.id} key={task.id}/>)
     }
   </>;
   return (
@@ -48,8 +58,7 @@ function TaskList(props) {
 }
 
 const mapStateToProps = (state) => ({
-  filteredTasks: getCurrentListTasks2(state),
-  filteredTasks2: getCurrentListTasks2(state)
+  filteredTasks: getCurrentListTasks2(state)
 });
 
 export default connect(mapStateToProps, null)(TaskList);
