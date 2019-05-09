c1a2c722f8a03ba9e8c11c9ac19322d765e7e04f
GrayStrider <zhoga.ivan@gmail.com>
2019-05-09 20:58:23 +1100

    wait function with async-reactor

--- src/app/components/TaskList/index.tsx
+++ src/app/components/TaskList/index.tsx
@@ -1,14 +1,34 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 
-import { map} from 'lodash';
+import { map } from 'lodash';
 import { Wrapper } from './styles';
 import Task from '../Task';
 import Scrollbar from '../Scrollbar';
-import { getCurrentListTasks, getCurrentListTasks2 } from 'app/components/TaskList/selectors';
+import { getCurrentListTasks2 } from 'app/components/TaskList/selectors';
+import { Loader } from 'semantic-ui-react';
+import { AsyncReactor } from '@ovotech/async-reactor-ts';
+
 
 function TaskList(props) {
   const { filteredTasks } = props;
+  const wait = ms => new Promise((r, j)=>setTimeout(r, ms))
+  const mapFilteredTasks = () => Promise.all(map(filteredTasks,
+    async (task) => (
+      <Task taskID={task.id} key={task.id}/>
+    )))
+  const asyncWrapper = (
+    <AsyncReactor
+      loader={async () => await wait(5000)}>
+      {({loading, result}) => {
+        if (loading) {
+          return <div>Loading...</div>
+        }
+
+        return <div>{result}</div>
+      }}
+    </AsyncReactor>
+  )
   const ListWrapper = <>
     {
       map(filteredTasks,
@@ -19,8 +39,9 @@ function TaskList(props) {
   </>;
   return (
     <Wrapper>
+      <Loader active={false}/>
       <Scrollbar style={{ height: '100%' }} autoHide>
-        {ListWrapper}
+        {asyncWrapper}
       </Scrollbar>
     </Wrapper>
   );
