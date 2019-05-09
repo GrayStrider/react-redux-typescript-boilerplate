2bc36d906f29621e3f54b6c1863201cd69fb6a82
GrayStrider <zhoga.ivan@gmail.com>
2019-05-09 23:39:05 +1100

    react-async some async works

--- src/app/components/TaskList/index.tsx
+++ src/app/components/TaskList/index.tsx
@@ -8,19 +8,39 @@ import { getCurrentListTasks2 } from 'app/components/TaskList/selectors';
 import { Loader } from 'semantic-ui-react';
 import { AsyncReactor } from '@ovotech/async-reactor-ts';
 import { Aigle } from 'aigle';
-import { list } from 'postcss';
+import { useAsync } from 'react-async';
+
+const _ = require('lodash');
 
 // @ts-ignore
-Aigle.mixin(require('lodash'));
+Aigle.mixin(_);
+const sleep = m => new Promise(r => setTimeout(r, m));
 
 function TaskList(props) {
   const { filteredTasks } = props;
-  const sleep = m => new Promise(r => setTimeout(r, m));
 
-  let asyncTasks = map(filteredTasks,
-    async (task) => (
-      <Task taskID={task.id} key={task.id}/>
-    ));
+  const asyncfn = async () => {
+    console.time('Slept for');
+    await sleep(3000);
+    console.timeEnd('Slept for');
+    return JSON.stringify(
+      filteredTasks
+    );
+  }
+
+  const MyComponent = () => {
+    const { data, error, isLoading } = useAsync({ promiseFn: asyncfn})
+    if (isLoading) return "Loading..."
+    if (error) return `Something went wrong: ${error.message}`
+    if (data)
+      return (
+        <div>
+          <strong>Loaded some data:</strong>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+      )
+    return null
+  }
 
   const asyncWrapper = (
     <AsyncReactor
@@ -47,11 +67,21 @@ function TaskList(props) {
         (task) => <Task taskID={task.id} key={task.id}/>)
     }
   </>;
+  const array = [1, 2, 3];
+  // @ts-ignore
+  const asyncMap = () => Aigle.map(array, n => Aigle.delay(10, n * 2)).sum()
+    .then(value => {
+      console.log(value); // 12
+    });
+
+
+
   return (
     <Wrapper>
       <Loader active={false}/>
       <Scrollbar style={{ height: '100%' }} autoHide>
-        {asyncWrapper}
+        // @ts-ignore
+        <MyComponent/>
       </Scrollbar>
     </Wrapper>
   );
